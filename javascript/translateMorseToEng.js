9bae97e7fb4ba90c39fef40bd5b4331e644e7b11
keeganfal <60140681+keeganfal@users.noreply.github.com>
2022-07-11 16:51:58 +0100

    morse to english done

--- javascript/translateMorseToEng.js
+++ javascript/translateMorseToEng.js
@@ -1,4 +1,38 @@
-export const translateMorseToEng = (morseStr) => {
-    let englishStr = morseStr;
-    return englishStr;
+import { morseCodeDict } from "./morseCode.js";
+
+const morseInput = document.querySelector("#morse-input")
+const morseBtn = document.querySelector("#morse-submit-btn")
+const morseOutput = document.querySelector(".main__output-morse")
+
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+export const translateMorseToEng = () => {
+    let morseStr = morseInput.value;
+    
+    let morseWordArr = morseStr.split(" / ")
+    console.log(morseWordArr);
+    let morseCharArr = [];
+    //let morseCharArr = [];
+    for (let i = 0; i < morseWordArr.length; i++){
+        morseCharArr[i] = morseWordArr[i].split(" ");
+    }  
+    console.log(morseCharArr);
+
+    //console.log(morseCharArr);
+
+    let englishStr = "";
+
+    for(let i = 0; i < morseCharArr.length; i++) {
+        for(let j = 0; j < morseCharArr[i].length; j++) {
+        englishStr +=  getKeyByValue(morseCodeDict, morseCharArr[i][j])
+        }
+        englishStr += " "
+    }
+    console.log(englishStr);
+    morseOutput.innerHTML = englishStr
+    return;
 };
+
+morseBtn.addEventListener("click", translateMorseToEng)
--- main.js
+++ main.js
@@ -1,1 +1,2 @@
 import { translateEngToMorse } from "./javascript/translateEngToMorse.js";
+import { translateMorseToEng } from "./javascript/translateMorseToEng.js";
